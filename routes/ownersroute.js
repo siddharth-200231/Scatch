8143c661fe6208ed7a2c8865b3897c4f6a1a2dfe
Siddharth <sahusidd715@gmail.com>
2024-08-29 11:10:16 +0530

    new file:   config/development.json 	modified:   routes/ownersroute.js

--- routes/ownersroute.js
+++ routes/ownersroute.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const owner=require("../models/owner-models")
 
 // Define routes
 router.get("/", (req, res) => {
   res.send("Welcome to the Owners Route");
 });
+// console.log(process.env)
+router.post("/",(req,res)=>{
+    res.send("it is working")
+})
 
 module.exports = router;
