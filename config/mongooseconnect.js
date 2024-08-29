153f4c03551ce3f4203ec0c7493dab81f1a68953
Siddharth <sahusidd715@gmail.com>
2024-08-29 09:17:12 +0530

    deleted:    config/.gitkeep 	modified:   config/mongooseconnect.js 	modified:   index.js 	deleted:    models/.gitkeep

--- config/mongooseconnect.js
+++ config/mongooseconnect.js
@@ -1,3 +1,7 @@
-const { default: mongoose } = require("mongoose")
-
-mongoose.connect("https://127.0.0.1:27017/scatch")
+const mongoose=require("mongoose")
+mongoose.connect("https://127.0.0.1:27017/scatch").then(()=>{
+    console.log("Mongoose Succesfully connected")
+}).catch((err)=>{
+    console.log(err)
+})
+module.exports=mongoose.connection
